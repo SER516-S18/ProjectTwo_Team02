b9fc0c27ae7560209a734b4c896fbc93c1af5dc4
jerss2k <dcarrol3@asu.edu>
2018-02-21 14:48:09 -0700

    wip

---
+++ Server/src/main/java/ConnectedClient.java
@@ -0,0 +1,9 @@
+import com.esotericsoftware.kryonet.Connection;
+
+/**
+ * Holds the channel number set by a connecting client
+ */
+public class ConnectedClient {
+    public Connection connection;
+    public int channelNum;
+}
