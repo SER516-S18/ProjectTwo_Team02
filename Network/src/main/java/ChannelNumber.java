e7ded63cb8e393049c168feb1bcd24f056711095
jerss2k <dcarrol3@asu.edu>
2018-02-21 14:48:23 -0700

    Connected server and client

--- Network/src/main/java/ChannelNumber.java
+++ Network/src/main/java/ChannelNumber.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+/**
+ * Represents a single channel and the associated random number
+ * to be sent over the network to the client
+ *
+ * @author Team2
+ * @version 1.0
+ */
+class ChannelNumber {
+    private int channel;
+    private int number;
 
-public class ChannelNumber {
+    // No arg constructor needed to init network
+    ChannelNumber(){}
+
+    ChannelNumber( int channel, int number ){
+        this.channel = channel;
+        this.number = number;
+    }
+
+    public int getChannel(){
+        return channel;
+    }
+
+    public int getNumber(){
+        return number;
+    }
 }
--- Server/src/main/java/ConnectedClient.java
+++ Server/src/main/java/ConnectedClient.java
@@ -1,9 +1,27 @@
-import com.esotericsoftware.kryonet.Connection;
-
 /**
  * Holds the channel number set by a connecting client
+ *
+ * @author Team2
+ * @version 1.0
  */
-public class ConnectedClient {
-    public Connection connection;
-    public int channelNum;
+class ConnectedClient {
+    private int connectionId;
+    private int channelNum;
+
+    ConnectedClient( int connectionId, int channelNum ){
+        this.connectionId = connectionId;
+        this.channelNum = channelNum;
+    }
+
+    public int getConnectionId() {
+        return connectionId;
+    }
+
+    public int getChannelNum() {
+        return channelNum;
+    }
+
+    public void setChannelNum(int channelNum) {
+        this.channelNum = channelNum;
+    }
 }
