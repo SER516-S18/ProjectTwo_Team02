f49fae2f246b8e0c2ec907a621d0b72743159f05
dcarrol3 <dcarrol3@asu.edu>
2018-02-23 21:54:46 -0700

    allow spaces in compile.bat

--- compile.bat
+++ compile.bat
@@ -14,7 +14,7 @@ del /f /Q client.jar
 echo Compiling Server
 
 
-javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.23.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\server\ServerApp.java
+javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.23.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" "%ROOT_PATH%src\server\ServerApp.java"
 
 jar cfm server.jar manifest_server.txt lib src server network
 del /f /Q server 
@@ -22,7 +22,7 @@ del /f /Q server
 echo Compiling Client
 
 
-javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.23.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\client\ClientApp.java
+javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.23.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" "%ROOT_PATH%src\client\ClientApp.java"
 
 jar cfm client.jar manifest_client.txt lib src client network
 del /f /Q client
